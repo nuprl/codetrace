4ac97d3c36983757940010ff4dec76852a96930c
flucchetti <lucchetti.f@northeastern.edu>
2023-04-27 12:01:01 -0400

    save

---
+++ .ipynb_checkpoints/model_loader-checkpoint.py
@@ -0,0 +1,41 @@
+import torch
+import transformers
+from tqdm import tqdm
+import transformers
+from baukit import nethook
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import re
+
+
+class ModelLoader:
+    def __init__(self, 
+                 MODEL_NAME,
+                 dtype = torch.float16 ) -> None:
+        
+        self.MODEL_NAME = MODEL_NAME
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            MODEL_NAME, low_cpu_mem_usage=True, torch_dtype=dtype,
+        )
+        self.model.eval().cuda()
+
+        # for n, p in self.model.named_parameters():
+        #     print(n, p.shape, p.device)
+
+        nethook.set_requires_grad(False, self.model)
+        if("gpt" in self.model.config.model_type):
+            self.tokenizer.pad_token = self.tokenizer.eos_token
+        elif(
+            "llama" in self.model.config._name_or_path or
+            "galactica" in self.model.config._name_or_path
+        ):
+            self.tokenizer.add_special_tokens({'pad_token': '[PAD]'}) 
+            
+        print(f"{MODEL_NAME} ==> device: {self.model.device}, memory: {self.model.get_memory_footprint()}")
+
+        self.layer_names = [
+            n
+            for n, m in self.model.named_modules()
+            if (re.match(r"^(transformer|gpt_neox)\.(h|layers)\.\d+$", n))
+        ]
+        self.num_layers = len(self.layer_names)
