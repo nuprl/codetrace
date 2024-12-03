14e8a578f6132b301cccc9fef5b3b13d5373814d
flucchetti <fr.lucchetti@gmail.com>
2024-12-03 08:52:41 -0500

    added wip plot activations by success

--- codetrace/scripts/plot_activations.py
+++ codetrace/scripts/plot_activations.py
@@ -22,12 +22,9 @@ import itertools as it
 import einops
 
 def _chain_activations(activations: List[HiddenStateStack_1tok], layer:int) -> np.ndarray:
-    numpy_activ = []
-    while len(activations) > 0:
-        activ = activations.pop()[:,layer].squeeze() # nsamples, nprompt, hdim
-        numpy_activ.append(activ)
-    numpy_activ = torch.stack(numpy_activ, dim=0).to(torch.float32)
-    return einops.rearrange(numpy_activ, "num_cases n_samples n_prompt hdim -> (num_cases n_samples n_prompt) hdim")
+    # n_layer, n_samples, hdim
+    activ = torch.concat(activations, dim=1).to(torch.float32)[layer].squeeze()
+    return activ.numpy() # n_samples, hdim
 
 def _tensorname(path: str) -> str:
     assert "steering" in path
@@ -37,40 +34,42 @@ def _tensorname(path: str) -> str:
             return n + ".pt"
 
 def plot_tsne(
-    labeled_activations: List[Tuple[datasets.Dataset, torch.Tensor]], 
+    labeled_activations: List[Tuple[datasets.Dataset, torch.Tensor, str]], 
     layers: List[int],
-    outfile: str
+    outfile: str,
+    random_state: int = None
 ):
-    tsne = TSNE(n_components=2, perplexity=30, random_state=42)
-    pca = PCA(n_components=50, random_state=42)
+    tsne = TSNE(n_components=2, perplexity=30, random_state=random_state)
+    pca = PCA(n_components=50, random_state=random_state)
 
     num_layers = len(layers)
-    fig, axes = plt.subplots(1, num_layers, figsize=(25, 25), constrained_layout=True)
+    fig, axes = plt.subplots(len(labeled_activations) // 2, num_layers, figsize=(15, 10), constrained_layout=True)
     
     if num_layers == 1:
         axes = [axes]
 
-    colors = ['red', 'blue', 'green']
+    colors = ['red', 'blue', 'green', 'yellow', 'orange', 'cyan', 'black']
 
-    for idx, layer in enumerate(layers):
-        ax = axes[idx]
-        data = _chain_activations([a for _, a in labeled_activations], layer)
-        data_pca = pca.fit_transform(data)
-        data_tsne = tsne.fit_transform(data_pca)
+    for j,activs in enumerate(zip(labeled_activations[::2], labeled_activations[1::2])):
+        for idx, layer in enumerate(layers):
+            ax = axes[j%(len(labeled_activations) // 2), idx]
+            data = _chain_activations([a for _, a,_ in activs], layer)
+            data_pca = pca.fit_transform(data)
+            data_tsne = tsne.fit_transform(data_pca)
 
-        print(data_tsne.shape)
-        n = 0
-        for i,(label, activations) in enumerate(labeled_activations):
-            interval = activations.shape[0]*activations.shape[2]
-            indices = list(range(n, n+interval)) # nsamples, nprompt
-            assert len(indices) != []
-            n += interval
-            color = colors[i]
-            print(color, label, layer, min(indices), max(indices))
-            ax.scatter(
-                data_tsne[indices, 0], data_tsne[indices, 1], 
-                c=color, label=f'Class {_tensorname(label)}', alpha=0.6
-            )
+            print(data_tsne.shape)
+            n = 0
+            for i,(label, activations, succ_label) in enumerate(activs):
+                interval = activations.shape[1]
+                indices = list(range(n, n+interval)) # nsamples, nprompt
+                assert len(indices) != []
+                n += interval
+                color = colors[i]
+                print(color, label, layer, min(indices), max(indices))
+                ax.scatter(
+                    data_tsne[indices, 0], data_tsne[indices, 1], 
+                    c=color, label=f'Class {_tensorname(label).replace(".pt", succ_label)}', alpha=0.6
+                )
         
         ax.set_title(f't-SNE Projection (Layer {layer})')
         ax.set_xlabel('t-SNE Dimension 1')
@@ -93,50 +92,51 @@ def main(
 ):
     for i in range(len(datasets)):
         ds = load_dataset(datasets[i])
+        ds = ds.map(lambda x: {**x, "success":x["steered_predictions"] == x["fim_type"]})
         ds._name = datasets[i]
         datasets[i] = ds
 
     model = LanguageModel(model, device_map="cuda", torch_dtype=torch.bfloat16, dispatch=True)
     labeled_activations = []
-    for sample,ds in enumerate(datasets):
-        activ_path = f"{cache_dir}/{_tensorname(ds._name)}"
-        print("--DATASET NAME--:", ds._name, activ_path)
-        assert len(ds) % batch_size == 0, "Dataset must be divisible by batch_size"
+    for sample,dataset in enumerate(datasets):
+        for succ_label in [True, False]:
+            ds = dataset.filter(lambda x: x["success"] == succ_label)
+            succ_label = "succ" if succ_label else "fail"
+            activ_path = f"{cache_dir}/{_tensorname(dataset._name).replace('.pt',succ_label + '.pt')}"
+            print("--DATASET NAME--:", activ_path)
 
-        if Path(activ_path).exists():
-            activ = torch.load(activ_path)
-        elif patches:
-            patch = torch.load(patches[sample])
-            activ_list = batched_patch(
-                model, 
-                list(ds[field]),
-                patch,
-                layers,
-                lambda x: mask_target_idx(x, [-1]),
-                masked_add,
-                reduction="sum",
-                batch_size=batch_size,
-                collect_hidden_states=layers
-            )
-            activ = torch.stack(activ_list, dim=0)
-            torch.save(activ, activ_path)
-        else:
-            activ_list = batched_collect_activations(
-                model, 
-                list(ds[field]),
-                lambda x: mask_target_idx(x, [-1]),
-                reduction="sum",
-                layers=layers,
-                batch_size=batch_size
-            )
-            activ = torch.stack(activ_list, dim=0)
-            torch.save(activ, activ_path)
+            if Path(activ_path).exists():
+                activ = torch.load(activ_path)
+            elif patches:
+                patch = torch.load(patches[sample])
+                activ_list = batched_patch(
+                    model, 
+                    list(ds[field]),
+                    patch,
+                    layers,
+                    lambda x: mask_target_idx(x, [-1]),
+                    masked_add,
+                    reduction="sum",
+                    batch_size=batch_size,
+                    collect_hidden_states=layers
+                )
+                activ = torch.concat(activ_list, dim=1)
+                torch.save(activ, activ_path)
+            else:
+                activ_list = batched_collect_activations(
+                    model, 
+                    list(ds[field]),
+                    lambda x: mask_target_idx(x, [-1]),
+                    reduction="sum",
+                    layers=layers,
+                    batch_size=batch_size
+                )
+                activ = torch.concat(activ_list, dim=1)
+                torch.save(activ, activ_path)
 
-        labeled_activations.append((ds._name, activ))
-        print(activ.shape)
+            labeled_activations.append((dataset._name, activ, succ_label))
+            print(activ.shape)
     
-    min_n_sample = min([a.shape[0] for _,a in labeled_activations])
-    labeled_activations = [(l,a[:min_n_sample]) for l,a in labeled_activations]
     plot_tsne(labeled_activations, layers, outfile)
     
 if __name__=="__main__":
