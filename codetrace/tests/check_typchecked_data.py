bef081434b08bf692cda3cc8cf3c54abc6e7f1ad
flucchetti <fr.lucchetti@gmail.com>
2024-12-04 10:47:22 -0500

    typecheck check

--- codetrace/tests/check_typchecked_data.py
+++ codetrace/tests/check_typchecked_data.py
@@ -2,25 +2,41 @@ import sys
 import datasets
 import pandas as pd
 
-ds = datasets.load_dataset("nuprl-staging/type-steering", name=sys.argv[1].split("/")[-1], split="train")
-ds_typechecked = datasets.load_from_disk(sys.argv[1])
-
-print(f"Lost {len(ds) - len(ds_typechecked)} dupes: {len(ds)} -> {len(ds_typechecked)}")
-
-df = ds.to_pandas()
-df_typechecked = ds_typechecked.to_pandas()
-print(f"Num typechecks {df_typechecked['typechecks'].sum()}/{len(df_typechecked)} = {df_typechecked['typechecks'].mean():.2f}")
-
 def compare(typechecked, original):
     cols = original.columns
-    typechecked["mutation_names"] = typechecked["mutation_names"].astype(str)
-    original["mutation_names"] = original["mutation_names"].astype(str)
     typechecked = typechecked[cols]
     return len(typechecked.merge(original)) == len(typechecked)
-    
 
-equals = compare(df_typechecked, df)
-assert equals
-print(equals)
-assert len(df_typechecked) >= 3000
+def _cast(x, feats):
+    new_x = {}
+    for k,v in x.items():
+        if k in feats:
+            new_x[k] = str(v)
+        else:
+            new_x[k] = v
+    return new_x
+
+def cast_feats(ds):
+    list_features = [k for k,v in ds.features.items() if "Sequence" in str(v)]
+    return ds.map(lambda x: _cast(x, list_features), num_proc=10, desc="Casting")
+
+if __name__ == "__main__":
+    path = sys.argv[1]
+
+    ds = datasets.load_dataset("nuprl-staging/type-steering", name=path.split("/")[-1], split="train")
+    ds_typechecked = datasets.load_from_disk(path)
+    ds = cast_feats(ds)
+    ds_typechecked = cast_feats(ds_typechecked)
+    
+    print(f"Lost {len(ds) - len(ds_typechecked)} dupes: {len(ds)} -> {len(ds_typechecked)}")
+
+    df = ds.to_pandas()
+    df_typechecked = ds_typechecked.to_pandas()
+    print(f"Num typechecks {df_typechecked['typechecks'].sum()}/{len(df_typechecked)} = {df_typechecked['typechecks'].mean():.2f}")
+
+    equals = compare(df_typechecked, df)
+    assert equals
+    print(f"Equality: {equals}")
+    assert len(df_typechecked) >= 3000
+    print(f"Length >= 3000: {len(df_typechecked) >= 3000}")
 
