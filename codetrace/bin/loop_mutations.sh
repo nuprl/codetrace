d106d0feb6c95f0001701f1920e407293706b22f
flucchetti <fr.lucchetti@gmail.com>
2024-12-05 04:29:40 -0500

    better loop_mutations.sh script

--- codetrace/bin/loop_mutations.sh
+++ codetrace/bin/loop_mutations.sh
@@ -3,13 +3,20 @@ MODEL=$1
 LANG=$2
 set -o pipefail
 
-ALL_MUTATIONS=("types,vars" "vars" "types" "delete" "delete,vars,types" "vars,delete" "types,delete")
+ALL_MUTATIONS=("vars,delete" "types,delete")
 
-counter=0
-while [ $counter -lt "${#ALL_MUTATIONS[@]}" ]; do
-    echo "Counter $counter"
+# Use an array to track processed mutations
+processed=()
+
+while [ "${#processed[@]}" -lt "${#ALL_MUTATIONS[@]}" ]; do
+    echo "Processed mutations: ${#processed[@]}"
 
     for MUTATIONS in "${ALL_MUTATIONS[@]}"; do
+        # Skip already processed mutations
+        if [[ " ${processed[@]} " =~ " $MUTATIONS " ]]; then
+            continue
+        fi
+
         MUTATIONSUNDERSCORED=$(echo $MUTATIONS | sed 's/,/_/g')
         echo $MUTATIONSUNDERSCORED
         file=/tmp/mutations-$LANG-$MUTATIONSUNDERSCORED-$MODEL.out
@@ -25,11 +32,22 @@ while [ $counter -lt "${#ALL_MUTATIONS[@]}" ]; do
             --batch-size 50 \
             --mutations=$MUTATIONS  >> $file 2>&1
 
-        grep -Eo "Collected [0-9]+ candidates" $file | awk '{if ($3 >= 3500) {exit 0} else {exit 1}}'
+        if [[ ! -s "$file" ]]; then
+            echo "ERROR: Log file $file is empty or unreadable"
+            continue
+        fi
+
+        grep_output=$(grep -Eo "Collected [0-9]+ candidates" $file)
+        if [[ -z "$grep_output" ]]; then
+            echo "No candidates collected in $file. Skipping..."
+            continue
+        fi
+
+        grep -Eo "Collected [0-9]+ candidates" $file | awk '{if ($3 ~ /^[0-9]+$/ && $3 >= 3500) {exit 0} else {exit 1}}'
 
         if [ $? -eq 0 ]; then
-            counter=$((counter + 1))
-            echo "Finished mutations $MUTATIONS, counter: $counter"
+            echo "Finished mutations $MUTATIONS"
+            processed+=("$MUTATIONS")  # Mark this mutation as processed
         else
             echo "Problem with mutations $MUTATIONS"
         fi
